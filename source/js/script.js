377ae107a57f9b101f527cdadcdb2b04b7333e7f
Cami <Camilo Sierra>
2022-01-04 19:04:12 -0500

    Responsive menu bar

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,12 @@
-var button = document.getElementById('firstButton') 
+function displayMenu(){
+    document.getElementById('flexBoxContainer').style.display = 'flex'
+    document.getElementById('displayManuRespNone').style.display = 'flex'
+    document.getElementById('flex-box-container').style.justifyContent = 'space-around'
+
+
+};
+function hideMenu() {
+    document.getElementById('flexBoxContainer').style.display = 'none'
+    document.getElementById('displayManuRespFlex').style.display = 'flex'
+    document.getElementById('displayManuRespNone').style.display = 'none'
+};
